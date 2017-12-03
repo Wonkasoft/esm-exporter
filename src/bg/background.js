9beda9a9baedf9e303de8bfcf070efead3a59ac0
MrLister <rlister@wonkasoft.com>
2017-12-02 21:52:54 -0800

    esm exporter fully working with button

--- src/bg/background.js
+++ src/bg/background.js
@@ -2,13 +2,7 @@
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
   });
 
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'inject.js'
-	});
-});
+
 
